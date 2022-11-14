efcb1e1ef3752309a19eba02af81bd8824711fd3
luan vendrami <luan.vendrami@hotmail.com>
2022-11-14 00:28:42 -0300

    Create a configuration of UnitOfWork for the project, will be used Dapper now.

---
+++ Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Data.Context;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.UnitOfWork
+{
+    public sealed class UnitOfWork : IUnitOfWork
+    {
+        private readonly ExodoDbSession _session;
+
+        public UnitOfWork(ExodoDbSession session)
+        {
+            _session = session;
+        }
+
+        public void BeginTransaction()
+        {
+            _session.Transaction = _session.Connection.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            _session.Transaction.Commit();
+            Dispose();
+        }
+
+        public void Rollback()
+        {
+            _session.Transaction.Rollback();
+            Dispose();
+        }
+
+        public void Dispose() => _session.Transaction?.Dispose();
+    }
+}
