0108954f80fa10d4bcaab2facc613e9a4c43752d
luan vendrami <luan.vendrami@hotmail.com>
2022-11-15 15:29:08 -0300

    commented validation in date

--- Simple.MEI.API/FluentValidation/ClientInputDtoValidator.cs
+++ Simple.MEI.API/FluentValidation/ClientInputDtoValidator.cs
@@ -8,28 +8,28 @@ namespace Simple.Client.MEI.API.FluentValidation
         public ClientInputDtoValidator()
         {
             RuleFor(x => x.FirstName)
-                .NotEmpty().WithMessage("\r\nPlease, fill in your first name.")
-                .NotNull().WithMessage("\r\nplease fill in your first name.");
+                .NotEmpty().WithMessage("Please, fill in your first name.")
+                .NotNull().WithMessage("please fill in your first name.");
 
             RuleFor(x => x.LastName)
-                .NotEmpty().WithMessage("\r\nPlease, fill in your last name.")
-                .NotNull().WithMessage("\r\nPlease, fill in your last name.");
+                .NotEmpty().WithMessage("Please, fill in your last name.")
+                .NotNull().WithMessage("Please, fill in your last name.");
 
             RuleFor(x => x.Cpf)
-                .NotEmpty().WithMessage("\r\nPlease, fill in your Cpf.")
-                .NotNull().WithMessage("\r\nPlease, fill in your Cpf.");
+                .NotEmpty().WithMessage("Please, fill in your Cpf.")
+                .NotNull().WithMessage("Please, fill in your Cpf.");
 
-            RuleFor(x => x.BirthDate)
-                .NotEmpty().WithMessage("\r\nPlease, fill in your birth date.")
-                .NotNull().WithMessage("\r\nPlease, fill in your birth date.");
+            //RuleFor(x => x.BirthDate)
+            //    .NotEmpty().WithMessage("Please, fill in your birth date.")
+            //    .NotNull().WithMessage("Please, fill in your birth date.");
 
             RuleFor(x => x.Email)
-                .NotEmpty().WithMessage("\r\nPlease, fill in your e-mail")
-                .NotNull().WithMessage("\r\nPlease, fill in your e-mail");
+                .NotEmpty().WithMessage("Please, fill in your e-mail")
+                .NotNull().WithMessage("Please, fill in your e-mail");
 
             RuleFor(x => x.Phone)
-                .NotEmpty().WithMessage("\r\nPlease, fill in your Phone number")
-                .NotNull().WithMessage("\r\nPlease, fill in your Phone number");
+                .NotEmpty().WithMessage("Please, fill in your Phone number")
+                .NotNull().WithMessage("Please, fill in your Phone number");
         }
     }
 }
