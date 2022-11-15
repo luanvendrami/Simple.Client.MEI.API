0fe17e1baac7f54d76e7bf8479e30bf35e7cf9a9
luan vendrami <luan.vendrami@hotmail.com>
2022-11-15 01:54:55 -0300

    Create FluentValidator for new input class Dto of client.

---
+++ Simple.MEI.API/FluentValidation/ClientInputDtoValidator.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Dto;
+using FluentValidation;
+
+namespace Simple.Client.MEI.API.FluentValidation
+{
+    public class ClientInputDtoValidator : AbstractValidator<ClientInputDto>
+    {
+        public ClientInputDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("\r\nPlease, fill in your first name.")
+                .NotNull().WithMessage("\r\nplease fill in your first name.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("\r\nPlease, fill in your last name.")
+                .NotNull().WithMessage("\r\nPlease, fill in your last name.");
+
+            RuleFor(x => x.Cpf)
+                .NotEmpty().WithMessage("\r\nPlease, fill in your Cpf.")
+                .NotNull().WithMessage("\r\nPlease, fill in your Cpf.");
+
+            RuleFor(x => x.BirthDate)
+                .NotEmpty().WithMessage("\r\nPlease, fill in your birth date.")
+                .NotNull().WithMessage("\r\nPlease, fill in your birth date.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("\r\nPlease, fill in your e-mail")
+                .NotNull().WithMessage("\r\nPlease, fill in your e-mail");
+
+            RuleFor(x => x.Phone)
+                .NotEmpty().WithMessage("\r\nPlease, fill in your Phone number")
+                .NotNull().WithMessage("\r\nPlease, fill in your Phone number");
+        }
+    }
+}
