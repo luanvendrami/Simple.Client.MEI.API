be862d1b9d85e2ea55ca96dbafa05d136411e444
luan vendrami <luan.vendrami@hotmail.com>
2022-11-15 23:02:10 -0300

    Changed the pick list base controller to string.

--- Simple.MEI.API/Controllers/BaseController.cs
+++ Simple.MEI.API/Controllers/BaseController.cs
@@ -6,23 +6,23 @@ namespace Simple.Client.MEI.API.Controllers
     [ApiController]
     public abstract class BaseController : ControllerBase
     {
-        protected IActionResult ResultOk(string message, object? objResult = null)
+        protected IActionResult ResultOk(List<string> message, object? objResult = null)
             => Ok(new Result(message, objResult));
-        protected IActionResult ResultBadRequest(string mensagem)
+        protected IActionResult ResultBadRequest(List<string> mensagem)
             => BadRequest(new Result(mensagem));
 
     }
     public class Result
     {
         public bool Success { get; private set; }
-        public string Message { get; private set; }
+        public List<string> Message { get; private set; }
         public object? IsResult { get; private set; }
-        public Result(string message)
+        public Result(List<string> message)
         {
             Success = false;
             Message = message;
         }
-        public Result(string message, object result)
+        public Result(List<string> message, object result)
         {
             Success = true;
             Message = message;
