006073dfedea717f1afd75e45042fc871b068561
luan vendrami <luan.vendrami@hotmail.com>
2022-11-15 23:00:02 -0300

    Alter flow to service, add any conditionals

--- Domain/Interfaces/Services/IClientService.cs
+++ Domain/Interfaces/Services/IClientService.cs
@@ -4,6 +4,6 @@ namespace Domain.Interfaces.Services
 {
     public interface IClientService
     {
-        Task<Tuple<string, bool>> Create(ClientInputDto clientInputDto);
+        Task<Tuple<List<string>, bool>> Create(ClientInputDto clientInputDto);
     }
 }
