33b8d3e1e9cb55bdbeb82c33b7d657565cc0d6e4
luan vendrami <luan.vendrami@hotmail.com>
2022-11-15 22:59:26 -0300

    Add new property and new class for result of massages.

--- Domain/Dto/ClientInputDto.cs
+++ Domain/Dto/ClientInputDto.cs
@@ -11,7 +11,7 @@ namespace Domain.Dto
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Cpf { get; set; }
-        //public DateTime BirthDate { get; set; }
+        public DateTime BirthDate { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
     }
