ee3a50f0a5977f64dbf876e991c53361aef66540
luan vendrami <luan.vendrami@hotmail.com>
2022-11-19 01:46:03 -0300

    Add new mehod signature in interface

--- Domain/Interfaces/Repository/IRepositoryClient.cs
+++ Domain/Interfaces/Repository/IRepositoryClient.cs
@@ -1,9 +1,11 @@
 ﻿using Domain.Models.RequestModels;
+using Domain.Models.ResponseModels;
 
 namespace Domain.Interfaces.Repository
 {
     public interface IRepositoryClient
     {
         Task<bool> Create(ClientRequestModel clientRequest);
+        Task<List<FetchClientResponseModel>> FetchCLient(ClientRequestModel clientRequestModel);
     }
 }
